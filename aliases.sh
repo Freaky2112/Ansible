e25a46c25e6aa311e2b9a7945066731e13c51c13
Freaky <freaky2112@hotmail.com>
2021-10-18 19:36:56 +0000

    repushing Aliases files

---
+++ aliases.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cat aliases.txt >> ~/.bashrc
+echo -e " ## My Aliases ## \n SSH AGENT { ssha } \n Distro Upgrade { upgrade } \n Install essentiel { git curl wget net-tools } \n What is  my ip { wip }\n"
