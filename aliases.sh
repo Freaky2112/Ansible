c7921beb08801990a525642fb58841c050d33c4d
freaky <freaky2112@hotmail.com>
2022-04-24 22:05:05 -0400

    Clean up in Files

--- aliases.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cat aliases.txt >> ~/.bashrc
-echo -e " ## My Aliases ## \n SSH AGENT { ssha } \n Distro Upgrade { upgrade } \n Install essentiel { git curl wget net-tools } \n What is  my ip { wip }\n"
